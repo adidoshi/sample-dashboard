a0b58ef6e69bbecc2cc34c80cef96d316b0afccb
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2021-10-11 23:35:03 +0530

    refactor: a to link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
       {/* <!-- Sidebar Toggle (Topbar) --> */}
       <button
-        id="sidebarToggleTop"
+        id="#sidebarToggleTop"
         className="btn btn-link d-md-none rounded-circle mr-3"
       >
         <i className="fa fa-bars"></i>
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,12 +7,12 @@ export default function Sidebar() {
             <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 {/* <!-- Sidebar - Brand --> */}
-<a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+<Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/">
     <div className="sidebar-brand-icon rotate-n-15">
         <i className="fas fa-laugh-wink"></i>
     </div>
     <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-</a>
+</Link>
 
 {/* <!-- Divider --> */}
 <hr className="sidebar-divider my-0" />
